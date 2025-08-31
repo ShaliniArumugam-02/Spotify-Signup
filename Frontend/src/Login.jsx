7d9407cf88b6a50a3b718b7564cbb8d3c285e60b
ShaliniArumugam-02 <shaliniarumugam003@gmail.com>
2025-08-31 18:52:07 +0530

    server updated

--- Frontend/src/Login.jsx
+++ Frontend/src/Login.jsx
@@ -16,7 +16,7 @@ const handleChange = (e)=>{
 const handleSubmit = async(e)=>{
   e.preventDefault();
 try {
-  const res = await fetch("http://localhost:5000/login", {
+  const res = await fetch("https://spotify-signup.onrender.com/login", {
     method:"POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify(form)
--- Frontend/src/Signup.jsx
+++ Frontend/src/Signup.jsx
@@ -60,7 +60,7 @@ const Signup = () => {
             return;
           }
         try {
-          const res = await fetch("http://localhost:5000/signup" , {
+          const res = await fetch("https://spotify-signup.onrender.com/signup" , {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body:JSON.stringify(form)
